5f1673a24d09b02ac677402fd9d8c52b6a9e09a0
User <User@DESKTOP-UEUUG6L>
2021-08-24 01:22:57 +0700

    Add project files.

---
+++ MultimediaApp/HeaderToImageConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace MultimediaApp
+{
+    /// <summary>
+    /// Converts a full path to a specific image type of a drive, folder or file
+    /// </summary>
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Get the full path
+            var path = (string)value;
+
+            // If the path is null, ignore
+            if (path == null)
+                return null;
+
+            //
+            var image = "Image/file.png"
+
+            return new BitmapImage(new Uri($"pack://application:,,,/iamge"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
